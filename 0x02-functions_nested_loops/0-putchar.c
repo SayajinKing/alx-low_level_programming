e7458e219cfcc9ee862c0a77d4d921ca3d5828b2
SayajinKing <yassineessaadi4@gmail.com>
2023-06-20 17:30:14 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
-int main() {
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the characters of the word "_putchar"
+ *              using the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	char word[] = "_putchar";
-	int i = 0 ;
+	int i = 0;
 
-	while (word[i] != '\0') {
+	while (word[i] != '\0')
+	{
 		putchar(word[i]);
 		i++;
 	}
 
 	putchar('\n');
 
-	return 0;
+	return (0);
 }	
