121bdd64cb85625e3ca8616eba988315f17df675
SayajinKing <yassineessaadi4@gmail.com>
2023-06-20 17:38:01 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,21 @@
 #include "main.h"
-
 /**
- * main - Entry point
- *
- * Description: Prints the characters of the word "_putchar"
- *              using the putchar function.
- *
- * Return: Always 0 (Success)
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
+
 int main(void)
 {
-	char word[] = "_putchar";
-	int i = 0;
-
-	while (word[i] != '\0')
-	{
-		putchar(word[i]);
-		i++;
-	}
-
-	putchar('\n');
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
-}	
+}
+	
